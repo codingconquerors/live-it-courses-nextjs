445d6bc8f4d459b6f1d966b8746958cb6a77cf70
khushwinder <chahalkhushwinder@gmail.com>
2022-05-24 00:50:27 -0400

    test

--- next.config.js
+++ next.config.js
@@ -5,15 +5,15 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app;
-  style-src 'self' 'unsafe-inline';
+  object-src 'self';
+  script-src 'self';
+  style-src 'self';
   img-src * blob: data:;
   media-src 'none';
   connect-src *;
   font-src 'self';
   frame-src giscus.app
 `
-
 const securityHeaders = [
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
   {
