dece5b248478b9934876b8a9efb2c0bae7efcf93
khushwinder <chahalkhushwinder@gmail.com>
2022-05-23 21:58:54 -0400

    adding comment sections

--- layouts/AuthorLayout.js
+++ layouts/AuthorLayout.js
@@ -1,5 +1,5 @@
-import SocialIcon from '@/components/social-icons'
 import Image from '@/components/Image'
+import Comments from '@/components/comments'
 import { PageSEO } from '@/components/SEO'
 
 export default function AuthorLayout({ children, frontMatter }) {
@@ -17,17 +17,11 @@ export default function AuthorLayout({ children, frontMatter }) {
         <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
           <div className="flex flex-col items-center pt-8">
             <Image src={avatar} alt="about us" width="300px" height="210px" />
-            {/* <h3 className="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight">{name}</h3>
-            <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
-            <div className="text-gray-500 dark:text-gray-400">{company}</div>
-            <div className="flex space-x-3 pt-6">
-              <SocialIcon kind="mail" href={`mailto:${email}`} />
-              <SocialIcon kind="github" href={github} />
-              <SocialIcon kind="linkedin" href={linkedin} />
-              <SocialIcon kind="twitter" href={twitter} />
-            </div> */}
           </div>
           <div className="prose max-w-none pt-8 pb-8 dark:prose-dark xl:col-span-2">{children}</div>
+          <div className="prose max-w-none pt-8 pb-8 dark:prose-dark xl:col-span-2">
+            <Comments frontMatter={frontMatter} />
+          </div>
         </div>
       </div>
     </>
--- layouts/WhyUs.js
+++ layouts/WhyUs.js
@@ -1,4 +1,4 @@
-import SocialIcon from '@/components/social-icons'
+import Comments from '@/components/comments'
 import Image from '@/components/Image'
 import { PageSEO } from '@/components/SEO'
 
@@ -17,17 +17,11 @@ export default function WhyUs({ children, frontMatter }) {
         <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
           <div className="flex flex-col items-center pt-8">
             <Image src={image} alt="why us" width="300px" height="210px" />
-            {/* <h3 className="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight">{name}</h3>
-            <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
-            <div className="text-gray-500 dark:text-gray-400">{company}</div>
-            <div className="flex space-x-3 pt-6">
-              <SocialIcon kind="mail" href={`mailto:${email}`} />
-              <SocialIcon kind="github" href={github} />
-              <SocialIcon kind="linkedin" href={linkedin} />
-              <SocialIcon kind="twitter" href={twitter} />
-            </div> */}
           </div>
           <div className="prose max-w-none pt-8 pb-8 dark:prose-dark xl:col-span-2">{children}</div>
+          <div className="prose max-w-none pt-8 pb-8 dark:prose-dark xl:col-span-2">
+            <Comments frontMatter={frontMatter} />
+          </div>
         </div>
       </div>
     </>
